33ea948b0cf840db8f90b62f85e742172e695430
LUOSYrrrr <125748002@qq.com>
2025-06-11 18:41:23 +1000

    update

--- site.config.js
+++ site.config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     bio: "I develop everything using node.",
     email: "luosylois@gmail.com",
     linkedin: "",
-    github: "LUOSYrrrrr",
+    github: "LUOSYrrrr",
     instagram: "",
   },
   projects: [
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://luosyrrrr.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
